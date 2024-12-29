7acc715b14cba40727bc63c3954abbf228f1e469
Lyumih <Lyumih@users.noreply.github.com>
2024-12-29 07:19:25 +0000

    Deploying to gh-pages from @ Lyumih/brand@8f1c5d2b54fdd986250ca2454a731859fce616db 🚀

--- web.mjs
+++ web.mjs
@@ -3591,7 +3591,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
