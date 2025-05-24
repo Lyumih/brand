8430f6b904ce017ab772440f354b87979e38e40b
Lyumih <Lyumih@users.noreply.github.com>
2025-05-24 07:22:03 +0000

    Deploying to gh-pages from @ Lyumih/brand@8f1c5d2b54fdd986250ca2454a731859fce616db 🚀

--- web.d.ts
+++ web.d.ts
@@ -1702,6 +1702,7 @@ declare namespace $ {
 	export class $mol_speck extends $mol_view {
 		theme( ): string
 		value( ): any
+		minimal_width( ): number
 		attr( ): ({ 
 			'mol_theme': ReturnType< $mol_speck['theme'] >,
 		})  & ReturnType< $mol_view['attr'] >
