860f3f24eb9bd312fb848193ef57254a5554dce7
Lyumih <Lyumih@users.noreply.github.com>
2026-03-17 07:57:30 +0000

    Deploying to gh-pages from @ Lyumih/brand@8f1c5d2b54fdd986250ca2454a731859fce616db 🚀

--- node.js
+++ node.js
@@ -5693,6 +5693,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
